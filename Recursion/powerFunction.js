5c384500405267a74952fa33bd8444fc62df38b2
pijush bera <pijushbera98@gmail.com>
2022-04-08 11:06:13 +0530

    pij

---
+++ Recursion/powerFunction.js
@@ -0,0 +1,13 @@
+//calculates the power of given base and exponent
+
+function power(base,exponent){
+    if(exponent===0){
+        return 1
+    }
+   
+    return base*power(base,exponent-1)
+   
+   
+   }
+
+console.log(power(2,3))
